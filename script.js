1981d2902e50736b8fccac93754e7666c74d9a11
Jay Bhatade <138126082+netspire1920@users.noreply.github.com>
2024-05-09 01:30:08 +0530

    update

--- script.js
+++ script.js
@@ -1,95 +1,119 @@
-// JavaScript
-(function ($) {
-    $.fn.countTo = function (options) {
-      options = options || {};
+// // JavaScript
+// (function ($) {
+//     $.fn.countTo = function (options) {
+//       options = options || {};
 
-      return $(this).each(function () {
-        var settings = $.extend({}, $.fn.countTo.defaults, {
-          from: $(this).data('from'),
-          to: $(this).data('to'),
-          speed: $(this).data('speed'),
-          refreshInterval: $(this).data('refresh-interval'),
-          decimals: $(this).data('decimals')
-        }, options);
+//       return $(this).each(function () {
+//         var settings = $.extend({}, $.fn.countTo.defaults, {
+//           from: $(this).data('from'),
+//           to: $(this).data('to'),
+//           speed: $(this).data('speed'),
+//           refreshInterval: $(this).data('refresh-interval'),
+//           decimals: $(this).data('decimals')
+//         }, options);
 
-        var loops = Math.ceil(settings.speed / settings.refreshInterval),
-          increment = (settings.to - settings.from) / loops;
+//         var loops = Math.ceil(settings.speed / settings.refreshInterval),
+//           increment = (settings.to - settings.from) / loops;
 
-        var self = this,
-          $self = $(this),
-          loopCount = 0,
-          value = settings.from,
-          data = $self.data('countTo') || {};
+//         var self = this,
+//           $self = $(this),
+//           loopCount = 0,
+//           value = settings.from,
+//           data = $self.data('countTo') || {};
 
-        $self.data('countTo', data);
+//         $self.data('countTo', data);
 
-        if (data.interval) {
-          clearInterval(data.interval);
-        }
-        data.interval = setInterval(updateTimer, settings.refreshInterval);
+//         if (data.interval) {
+//           clearInterval(data.interval);
+//         }
+//         data.interval = setInterval(updateTimer, settings.refreshInterval);
 
-        render(value);
+//         render(value);
 
-        function updateTimer() {
-          value += increment;
-          loopCount++;
+//         function updateTimer() {
+//           value += increment;
+//           loopCount++;
 
-          render(value);
+//           render(value);
 
-          if (typeof(settings.onUpdate) == 'function') {
-            settings.onUpdate.call(self, value);
-          }
+//           if (typeof(settings.onUpdate) == 'function') {
+//             settings.onUpdate.call(self, value);
+//           }
 
-          if (loopCount >= loops) {
-            $self.removeData('countTo');
-            clearInterval(data.interval);
-            value = settings.to;
+//           if (loopCount >= loops) {
+//             $self.removeData('countTo');
+//             clearInterval(data.interval);
+//             value = settings.to;
 
-            if (typeof(settings.onComplete) == 'function') {
-              settings.onComplete.call(self, value);
-            }
+//             if (typeof(settings.onComplete) == 'function') {
+//               settings.onComplete.call(self, value);
+//             }
 
-            // Add this line to stop the interval after the first run
-            clearInterval(data.interval);
-          }
-        }
+//             // Add this line to stop the interval after the first run
+//             clearInterval(data.interval);
+//           }
+//         }
 
-        function render(value) {
-          var formattedValue = settings.formatter.call(self, value, settings);
-          $self.html(formattedValue);
-        }
-      });
-    };
+//         function render(value) {
+//           var formattedValue = settings.formatter.call(self, value, settings);
+//           $self.html(formattedValue);
+//         }
+//       });
+//     };
 
-    $.fn.countTo.defaults = {
-      from: 0,
-      to: 1000,
-      speed: 1000,
-      refreshInterval: 100,
-      decimals: 0,
-      formatter: formatter,
-      onUpdate: null,
-      onComplete: null
-    };
+//     $.fn.countTo.defaults = {
+//       from: 0,
+//       to: 1000,
+//       speed: 1000,
+//       refreshInterval: 100,
+//       decimals: 0,
+//       formatter: formatter,
+//       onUpdate: null,
+//       onComplete: null
+//     };
 
-    function formatter(value, settings) {
-      return value.toFixed(settings.decimals);
-    }
-  }(jQuery));
+//     function formatter(value, settings) {
+//       return value.toFixed(settings.decimals);
+//     }
+//   }(jQuery));
 
-  jQuery(function ($) {
-    var observer = new IntersectionObserver(function (entries) {
-      if (entries[0].isIntersecting) {
-        $('.timer').each(function () {
-          var $this = $(this);
-          var options = $.extend({}, $this.data('countToOptions') || {});
-          $this.countTo(options);
-        });
+//   jQuery(function ($) {
+//     var observer = new IntersectionObserver(function (entries) {
+//       if (entries[0].isIntersecting) {
+//         $('.timer').each(function () {
+//           var $this = $(this);
+//           var options = $.extend({}, $this.data('countToOptions') || {});
+//           $this.countTo(options);
+//         });
 
-        // Add this line to disconnect the observer after the first run
-        observer.disconnect();
+//         // Add this line to disconnect the observer after the first run
+//         observer.disconnect();
+//       }
+//     }, { threshold: 1.0 });
+
+//     observer.observe(document.getElementById('intersect2'));
+//   });
+
+
+$(document).ready(function () {
+  var silder = $(".owl-carousel");
+  silder.owlCarousel({
+      autoPlay: false,
+      items: 1,
+      center: false,
+      nav: true,
+      margin: 40,
+      dots: false,
+      loop: true,
+      navText: ["<i class='fa fa-arrow-left' aria-hidden='true'></i>", "<i class='fa fa-arrow-right' aria-hidden='true'></i>"],
+      responsive: {
+          0: {
+              items: 1,
+          },
+          575: { items: 1 },
+          768: { items: 2 },
+          991: { items: 3 },
+          1200: { items: 4 }
       }
-    }, { threshold: 1.0 });
-
-    observer.observe(document.getElementById('intersect2'));
   });
+});
