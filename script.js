64fe3262cd3b7a83a68dcfe4db01043729f41a5b
Jay Bhatade <138126082+netspire1920@users.noreply.github.com>
2024-05-14 11:37:20 +0530

    finalize home page

--- script.js
+++ script.js
@@ -97,3 +97,22 @@
 
 
 
+
+
+const backToTop = document.getElementById("back-to-top");
+const page2 = document.getElementById("page2");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > page2.offsetTop) {
+    backToTop.classList.remove("d-none");
+  } else {
+    backToTop.classList.add("d-none");
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
